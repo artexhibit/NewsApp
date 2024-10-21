49e7f1ef30c1768768acc4d62d4880774d2bf952
artexhibit <vendabal@yandex.ru>
2024-10-21 22:36:54 +0300

    add Splash Screen

--- app/src/main/java/ru/igorcodes/newsapp/MainActivity.kt
+++ app/src/main/java/ru/igorcodes/newsapp/MainActivity.kt
@@ -3,16 +3,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.igorcodes.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        showSplashScreenAfter()
+
         setContent {
             NewsAppTheme {
-
+                // Your Compose content goes here
             }
+        }
+    }
+
+    private fun showSplashScreenAfter(seconds: Long = 2000) {
+        val splashScreen = installSplashScreen()
+        var isSplashScreenVisible = true
+
+        splashScreen.setKeepOnScreenCondition { isSplashScreenVisible }
+
+        lifecycleScope.launch {
+            delay(seconds)
+            isSplashScreenVisible = false
         }
     }
 }
